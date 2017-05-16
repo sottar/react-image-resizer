9780fa5e7d3c2a3cf83eed3cfec79de87559026a
sottar <ohrst.18@gmail.com>
2017-05-16 10:14:32 +0900

    first implementation

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.resizeImage = this.resizeImage.bind(this);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  resizeImage() {
+    if (ReactDOM.findDOMNode(this.refs.image) == null) {
+      return;
+    }
+    const originalWidth = ReactDOM.findDOMNode(this.refs.image).naturalWidth;
+    const originalHeight = ReactDOM.findDOMNode(this.refs.image).naturalHeight;
+    const widthRatio = this.props.width / originalWidth;
+    const heightRatio = this.props.height / originalHeight;
+    if (widthRatio < heightRatio) {
+      this.setState({
+        width: originalWidth * widthRatio,
+        height: originalHeight * heightRatio,
+      });
+    } else {
+      this.setState({
+        width: originalWidth * heightRatio,
+        height: originalHeight * heightRatio,
+      });
+    }
+  }
+
+  render() {
+    const style = {
+      wrapper: {
+        backgroundColor: this.props.backgroundColor,
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      },
+      image: {
+        position: 'absolute',
+        display: 'block',
+        left: (this.props.width - this.state.width) / 2,
+        top: (this.props.height - this.state.height) / 2,
+        width: this.state.width,
+        height: this.state.height,
+      },
+    };
+    return (
+      <div style={style.wrapper}>
+        <img
+          ref="image" src={this.props.src} alt="" style={style.image}
+          onLoad={this.resizeImage}
+         />
+      </div>
+    );
+  }
+}
+
+Image.PropTypes = {
+  src: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  backgroundColor: PropTypes.string,
+};
